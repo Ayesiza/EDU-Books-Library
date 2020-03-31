282b864c765fdc720f26cf785fd0b50427e958ec
Ayesiza Hawah <yesiha2001@gmail.com>
2020-03-31 17:56:59 +0300

    chore(changes on signup)

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,28 +1,30 @@
 
-document.getElementById("myform").addEventListener("click", function(event){
-    event.preventDefault()
-  });
-  
-formValidation=(e)=>{
+
+formValidation=()=>{
     
     const name = document.getElementById('name').value;
    const email = document.getElementById('email').value;
    const password = document.getElementById('pswd').value;
 if(name == "" || password =="" || email ==""){
-document.getElementById("foresult").innerhtml = "All fields are required";
+ alert("All fields are required");
   return false;
 }
 if(name.length < 3){
-  document.getElementById(foresult).innerHTML = "Name field must be atleast 3 characters";
+  alert ("Name field must be atleast 3 characters");
 }
-if(email.value !== "[^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$]"){ 
-  document.getElementById(foresult).innerHTML = "The string must contain at least one special character";
+if(email.length < 5){ 
+  alert("Email must contain at least one special character");
 }
-else if(password.value !== "[^(?=.*\d).{4,8}$)]") {
-  document.getElementById(foresult).innerHTML = "Password must be between 4 and 8 digits long and include at least one numeric digit.";
+else if(password.value <5) {
+  alert("Password must be atleast 5 characters.");
 }
 else{
+  
+  alert("You have sucessfully registered");
+  window.location.replace('./login.html');
   return true
 }
  
   };
+  
+       
