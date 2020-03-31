76a7c9d101a263cfed04dca77b3f105c18391933
Ayesiza Hawah <yesiha2001@gmail.com>
2020-03-31 23:35:26 +0300

    ch(locate user role on login)

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,30 +1,26 @@
-
-
-formValidation=()=>{
-    
-    const name = document.getElementById('name').value;
-   const email = document.getElementById('email').value;
-   const password = document.getElementById('pswd').value;
-if(name == "" || password =="" || email ==""){
- alert("All fields are required");
-  return false;
-}
-if(name.length < 3){
-  alert ("Name field must be atleast 3 characters");
-}
-if(email.length < 5){ 
-  alert("Email must contain at least one special character");
-}
-else if(password.value <5) {
-  alert("Password must be atleast 5 characters.");
-}
-else{
+document.getElementById('myform').addEventListener("submit", event => {
+  // stop page from refreshing
+  event.preventDefault();
+  // get values from the form
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('pswd').value;
   
-  alert("You have sucessfully registered");
-  window.location.replace('./login.html');
-  return true
-}
- 
-  };
-  
+  if (name.length < 1 )  {
+    document.getElementById('helper-text').innerHTML =
+      "All fields are required";
+  }
+  if (email.length < 4 ) {
+    document.getElementById('helper-text').innerHTML =
+     "Email must contain at least one special character (@ &.)!";
+  } 
+     if (password.length < 4 ) {
+    document.getElementById('helper-text').innerHTML =
+     "Password must contain at least one uppercase letter (A-Z)!";
+  } 
+  else {
+    window.location.replace("./login.html");
+  }
+
+});
        
